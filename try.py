3c87a1c2ed9b6b81371a589656824854cea4804c
davidgmoconnor <dgmoc1@googlemail.com>
2018-08-29 09:38:41 +0100

    update codes

--- try.py
+++ try.py
@@ -1,16 +1,18 @@
 from pyluxafor import LuxaforFlag
 from time import sleep
 
+import codes as c
+
 flag = LuxaforFlag()
 flag.off()
 flag.fade_colour(
-    leds=[LuxaforFlag.LED_TAB_1, LuxaforFlag.LED_BACK_1, LuxaforFlag.LED_BACK_2],
+    leds=[c.LED_TAB_1, c.LED_BACK_1, c.LED_BACK_2],
     r=10, g=10, b=0,
     duration=255
 )
-flag.static_colour(leds=LuxaforFlag.LED_BACK_3, r=0, g=0, b=100)
+flag.static_colour(leds=c.LED_BACK_3, r=0, g=0, b=100)
 
 sleep(3)
 flag.off()
 
-flag.pattern(LuxaforFlag.PATTERN_POLICE, 3)
+flag.pattern(c.PATTERN_POLICE, 3)
